595c82319fdd2f77d6f76fcb044ef60c9e001c51
katerina <katpaltoglou@gmail.com>
2019-09-19 15:58:16 +0300

    update tranpiled_namedExports

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.tpl = undefined;
+
+var _fs = require("fs");
+
+var _fs2 = _interopRequireDefault(_fs);
+
+var _path = require("path");
+
+var _path2 = _interopRequireDefault(_path);
+
+var _utilsMerge = require("utils-merge");
+
+var _utilsMerge2 = _interopRequireDefault(_utilsMerge);
+
+var _analyse = require("./lib/analyse");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 var exported_build = build;
-import fs from "fs";
-import path from "path";
-import merge from "utils-merge";
-import { analyse } from "./lib/analyse";
+
 /**
  * Modified from grunt-yomb
  */
@@ -17,15 +37,15 @@ function encode(str) {
 function build(tmpl, opt) {
   opt = opt || {};
   opt.deps = opt.deps || {};
-  var res = []
-    , strict = opt.strict
-    , retFun = opt.ret === 'function'
-    , type = opt.type || 'html'
-    , unthrow = opt.unthrow || false;
+  var res = [],
+      strict = opt.strict,
+      retFun = opt.ret === 'function',
+      type = opt.type || 'html',
+      unthrow = opt.unthrow || false;
 
   if (opt.safe) {
     try {
-      analyse(tmpl, opt.path);
+      (0, _analyse.analyse)(tmpl, opt.path);
     } catch (e) {
       if (unthrow) {
         // res.push(
@@ -42,56 +62,25 @@ function build(tmpl, opt) {
         //   new Function('it', 'opt', res.join('')) : 
         //   res.join('');
       } else {
-        console.log([
-          '',
-          'template must have a error:',
-          e
-        ].join('\n'));
+        console.log(['', 'template must have a error:', e].join('\n'));
         throw new Error('template build error.');
       }
     }
-  } 
+  }
 
   tmpl.replace(/<\/script>/ig, '</s<%=""%>cript>');
-  res.push([
-    retFun ? undefined : "function (it, opt) {",
-    "    it = it || {};",
-    strict ?
-      "" :
-      "    with(it) {",
-    "        var _$out_= [];",
-    "        _$out_.push('" + tmpl
-      .replace(/\r\n|\n|\r/g, "\v")
-      .replace(/(?:^|%>).*?(?:<%|$)/g, function ($0) {
-        return type === 'html' ?
-          $0.replace(/('|\\)/g, "\\$1").replace(/[\v\t]/g, "").replace(/\s+/g, " ") :
-          $0.replace(/('|\\)/g, "\\$1").replace(/ +/g, " ").replace(/[\v\t]/g, "', '\\n', '");
-      })
-      .replace(/<!--[\s\S]+?-->/g, '')
-      .replace(/[\v]/g, EOL)
-      .replace(/<%=include\((['"])(.*?)\1\)(\(.*?\))%>/g, function ($0, $1, $2, $3) {
-        var file = path.join(path.dirname(opt.path), $2)
-          , newOpt = merge({}, opt);
-        if (opt.deps[file]) throw new Error('Do not circular reference, Please');
-        newOpt.path = file;
-        opt.deps[file] = true;
-        return "', " + build(fs.readFileSync(file, { encoding: 'utf8' }), newOpt) + $3 + ", '";
-      })
-      .replace(/<%==(.*?)%>/g, "', opt.encodeHtml($1), '")
-      .replace(/<%=(.*?)%>/g, "', $1, '")
-      .replace(/<%(<-)?/g, "');" + EOL + "      ")
-      .replace(/->(\w+)%>/g, EOL + "      $1.push('")
-      .split("%>").join(EOL + "      _$out_.push('") + "');",
-    "        return _$out_.join('');",
-    strict ?
-      "" :
-      "    }",
-    retFun ? undefined : "}"
-  ].join(EOL).replace(/_\$out_\.push\(''\);/g, ''));
+  res.push([retFun ? undefined : "function (it, opt) {", "    it = it || {};", strict ? "" : "    with(it) {", "        var _$out_= [];", "        _$out_.push('" + tmpl.replace(/\r\n|\n|\r/g, "\v").replace(/(?:^|%>).*?(?:<%|$)/g, function ($0) {
+    return type === 'html' ? $0.replace(/('|\\)/g, "\\$1").replace(/[\v\t]/g, "").replace(/\s+/g, " ") : $0.replace(/('|\\)/g, "\\$1").replace(/ +/g, " ").replace(/[\v\t]/g, "', '\\n', '");
+  }).replace(/<!--[\s\S]+?-->/g, '').replace(/[\v]/g, EOL).replace(/<%=include\((['"])(.*?)\1\)(\(.*?\))%>/g, function ($0, $1, $2, $3) {
+    var file = _path2.default.join(_path2.default.dirname(opt.path), $2),
+        newOpt = (0, _utilsMerge2.default)({}, opt);
+    if (opt.deps[file]) throw new Error('Do not circular reference, Please');
+    newOpt.path = file;
+    opt.deps[file] = true;
+    return "', " + build(_fs2.default.readFileSync(file, { encoding: 'utf8' }), newOpt) + $3 + ", '";
+  }).replace(/<%==(.*?)%>/g, "', opt.encodeHtml($1), '").replace(/<%=(.*?)%>/g, "', $1, '").replace(/<%(<-)?/g, "');" + EOL + "      ").replace(/->(\w+)%>/g, EOL + "      $1.push('").split("%>").join(EOL + "      _$out_.push('") + "');", "        return _$out_.join('');", strict ? "" : "    }", retFun ? undefined : "}"].join(EOL).replace(/_\$out_\.push\(''\);/g, ''));
 
-  return retFun ? 
-    new Function('it', 'opt', res.join('')) : 
-    res.join('');
+  return retFun ? new Function('it', 'opt', res.join('')) : res.join('');
 }
 
-export { exported_build as tpl };
+exports.tpl = exported_build;

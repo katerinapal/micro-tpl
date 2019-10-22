fa15ded15c922f649ee166e8620ac14f8672105f
katerina <katpaltoglou@gmail.com>
2019-10-22 11:31:02 +0300

    update branch

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,21 @@
-import gulp from "gulp";
-import jscs from "gulp-jscs";
-import mocha from "gulp-mocha";
+var _gulp = require("gulp");
 
-gulp.task('mocha', function () {
-  return gulp.src('./test/*.js', { read: false })
-    .pipe(mocha({ reporter: 'list' }));
-})
+var _gulp2 = _interopRequireDefault(_gulp);
 
-gulp.task('default', ['mocha'], function () {
-  return gulp.src(['index.js', './lib/**/*.js'])
-    .pipe(jscs());
+var _gulpJscs = require("gulp-jscs");
+
+var _gulpJscs2 = _interopRequireDefault(_gulpJscs);
+
+var _gulpMocha = require("gulp-mocha");
+
+var _gulpMocha2 = _interopRequireDefault(_gulpMocha);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_gulp2.default.task('mocha', function () {
+  return _gulp2.default.src('./test/*.js', { read: false }).pipe((0, _gulpMocha2.default)({ reporter: 'list' }));
+});
+
+_gulp2.default.task('default', ['mocha'], function () {
+  return _gulp2.default.src(['index.js', './lib/**/*.js']).pipe((0, _gulpJscs2.default)());
 });
